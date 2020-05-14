232c64011d712ad125b83c175c44719315b149d0
Elena Lepilkina <Elena.Lepilkina@jetbrains.com>
2020-05-14 11:44:48 +0300

    Update and changed options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,21 @@
 plugins {
-    kotlin("multiplatform") version "1.3.72"
-    kotlin("plugin.serialization") version "1.3.72"
+    kotlin("multiplatform") version "1.4-M1"
+    kotlin("plugin.serialization") version "1.4-M1"
 }
 repositories {
     mavenCentral()
+    maven ( url = "https://kotlin.bintray.com/kotlinx" )
+    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
 }
 
 group = "com.example"
 version = "0.0.1"
-val serializationRtVersion = "0.20.0"
+val serializationRtVersion = "0.20.0-1.4-M1"
 
 kotlin {
     jvm()
     iosX64()
+    macosX64()
 
     targets.all {
         compilations.all {
@@ -21,9 +24,17 @@ kotlin {
             }
         }
     }
+    macosX64().binaries.getTest("DEBUG").freeCompilerArgs += arrayOf("-opt", "-Xallocator=mimalloc")
+    iosX64().binaries.getTest("DEBUG").freeCompilerArgs += arrayOf("-opt", "-Xallocator=mimalloc")
 
     sourceSets {
         val iosX64Main by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serializationRtVersion")
+            }
+        }
+
+        val macosX64Main by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$serializationRtVersion")
             }
